188ab47260042427b4c8818c8c145e114b623806
mprabs <acharyaprabin03@gmail.com>
2020-02-19 05:52:18 -0500

    requests api integration

--- src/components/requestItem.vue
+++ src/components/requestItem.vue
@@ -4,18 +4,16 @@
       <v-toolbar-title>Request Item</v-toolbar-title>
     </v-toolbar>
     <v-data-table
-      :headers="requestHeader"
-      :items="newRequests"
-      class="elevation-1"
-      fixed-header
-      height="auto"
-    >
+  :headers="requestHeader"
+  :items="uniqueItem"
+  >
+
       <template v-slot:item.action="{ item }">
         <v-icon color="green" @click="verify(item)">mdi-check</v-icon>
         <v-icon color="red" @click="reject(item)">mdi-close</v-icon>
       </template>
+</v-data-table>
 
-    </v-data-table>
   </div>
 </template>
 <script>
@@ -25,12 +23,13 @@ export default {
       requestHeader: [
         {
           text: 'Employee Name',
-          value: 'employee.user.username'
+          value: 'username'
         },
-        { text: 'Equipment Name', value: 'abc' },
+        { text: 'Equipment Name', value: 'item' },
         { text: 'Action', value: 'action' }
       ],
       newRequests: [],
+      uniqueItem: [],
       dataUrl: 'http://127.0.0.1:8000/api/itemrequest/'
     }
   },
@@ -39,15 +38,32 @@ export default {
       .get(this.dataUrl)
       .then(response => {
         this.newRequests = response.data
+        this.displayItems()
       })
-  },
-  computed () {
   },
   methods: {
     verify (item) {},
     reject (item) {
       const index = this.newRequests.indexOf(item)
       confirm('Reject request?') && this.newRequests.splice(index, 1)
+    },
+    displayItems () {
+      this.newRequests.forEach(element => {
+        if (element.item.length > 1) {
+          for (let index = 0; index < element.item.length; index++) {
+            this.uniqueItem.push({
+              username: element.employee.user.username,
+              item: element.item[index].name
+            })
+          }
+        } else {
+          this.uniqueItem.push({
+            username: element.employee.user.username,
+            item: element.item[0].name
+          })
+        }
+      })
+      console.log('element', this.uniqueItem)
     }
   }
 }
