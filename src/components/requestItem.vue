10dd158be72bfd6bb98980bc5682b635d1167193
mprabs <acharyaprabin03@gmail.com>
2020-02-20 04:56:07 -0500

    rejection

--- src/components/requestItem.vue
+++ src/components/requestItem.vue
@@ -71,6 +71,12 @@ export default {
     reject (item) {
       const index = this.newRequests.indexOf(item)
       confirm('Reject request?') && this.newRequests.splice(index, 1)
+      console.log('item', item)
+      this.$axios.post('http://127.0.0.1:8000/api/rejectrequest/', {
+        employee: item.userId,
+        item: item.itemId
+      })
+      this.closeSheet()
       this.snackbar = true
       this.info = 'request rejected'
     },
